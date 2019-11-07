f92c888ba299017a4df41828630d80a29f284a25
udeep <udeep@nagarro.com>
2019-11-07 11:59:29 +0530

    updated config server

--- config-service/src/main/java/com/nagarro/microservices/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/nagarro/microservices/configservice/ConfigServiceApplication.java
@@ -1,8 +1,10 @@
-package com.nagp.configservice;
+package com.nagarro.microservices.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServiceApplication {
 
