5fc71df3346b1d3e5098c3eed11580eb36ea0108
udeep <udeep@nagarro.com>
2019-11-08 10:20:53 +0530

    updated code with config server and different properties file

--- user-service/user-service/src/main/java/com/nagp/userservice/controller/UserServiceController.java
+++ user-service/user-service/src/main/java/com/nagp/userservice/controller/UserServiceController.java
@@ -23,6 +23,11 @@ public class UserServiceController {
 	@Autowired
 	private UserService userService;
 	
+	@GetMapping("/test")
+	public Integer test(){
+		return 100;
+	}
+	
 	@GetMapping("/user/{emailId}/{password}")
 	public String getUserByEmailAndPassword(@PathVariable String emailId, @PathVariable String password) throws Exception{
 		Gson gson = new Gson();
